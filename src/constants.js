e028724c14164e1fafb6052d3cc05590fcf3c6b9
Barun Aaryan <barunaryan1@gmail.com>
2024-05-04 16:04:29 +0530

    First approach to connect to DB

---
+++ src/constants.js
@@ -0,0 +1,2 @@
+export const DB_NAME= "videotube"
+//not so sensitive so we stored it here
---
+++ src/index.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
+
+
+
+
+
+
+
+
+
+
+/*
+import express from "express"
+
+const app = express()
+
+// function connetctDB(){}
+
+// connetctDB()
+
+
+(async()=> {
+    try{
+await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+app.on("error", (error)=>{
+    console.log("Error is ", error);
+    throw error
+})
+app.listen(process.env.PORT , ()=>{
+    console.log(`App is listening on the port${process.env.PORT}` );
+})
+    }catch(error){
+console.log("ERROR is: " , error)
+throw err
+    }
+}) ()
+*/
